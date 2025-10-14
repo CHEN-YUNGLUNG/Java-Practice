4ab5ad79b4808f678014755dc4e267ba659fa41b
CHEN-YUNGLUNG <stu89572@gmail.com>
2025-10-15 00:01:31 +0900

    スッキリわかるJava入門第４版-Chapter１練習

--- Chapter01/Practice.java
+++ Chapter01/Practice.java
@@ -4,7 +4,7 @@ public class Practice {
     public static void main(String[] args){
         boolean b=true;
         char c='駆';
-        double d=3.14;
+        double d=3.141;
         long l=314159265853979L;
         String s="ミナトの攻撃！敵に15ポイントのダメージを与えた。";
         System.out.println(b);
